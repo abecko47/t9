6d702f6a5e5db9393c4fd8f3843fc33f8ebbe420
anatoliy <anatoliy@eyerim.com>
2022-01-30 23:47:36 +0100

    init fe

---
+++ packages/frontend/src/service/ApiService.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const url = 'http://127.0.0.1:3001'
+
+export const ApiService = () => {
+  const getWords = (numbers:string, words:number) => {
+
+  }
+
+  return {
+      getWords
+  }
+}
